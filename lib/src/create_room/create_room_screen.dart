63629148a665c8e361e74589fd748270658673cb
orangeman7201 <aki.jazz1027@gmail.com>
2023-01-03 18:18:06 +0900

    大まかなレイアウトを作成

--- lib/src/create_room/create_room_screen.dart
+++ lib/src/create_room/create_room_screen.dart
@@ -4,11 +4,11 @@ import 'package:provider/provider.dart';
 
 import '../audio/audio_controller.dart';
 import '../audio/sounds.dart';
-// import '../player_progress/player_progress.dart';
 import '../style/palette.dart';
 
 class CreateRoomScreen extends StatelessWidget {
   const CreateRoomScreen();
+  static const _gap = SizedBox(height: 20);
 
   @override
   Widget build(BuildContext context) {
@@ -19,45 +19,50 @@ class CreateRoomScreen extends StatelessWidget {
         backgroundColor: palette.backgroundMain,
         body: Container(
           width: double.infinity,
-          child: Column(
-            children: [
-              SizedBox(height: 30),
-              Container(
-                alignment: Alignment.center,
-                height: 220,
-                child: Transform.rotate(
-                  angle: -0.1,
-                  child: const Text(
-                    'Buttle line',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontFamily: 'Permanent Marker',
-                      fontSize: 55,
-                      height: 1,
-                    ),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(100, 50, 100, 0),
+            child: Column(
+              children: [
+                TextField(
+                  autofocus: true,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: const InputDecoration(
+                    border: UnderlineInputBorder(),
+                    labelText: '部屋の名前',
                   ),
                 ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      audioController.playSfx(SfxType.buttonTap);
-                      GoRouter.of(context).push('/create_room');
-                    },
-                    child: const Text('作成'),
+                _gap,
+                TextField(
+                  decoration: const InputDecoration(
+                    border: UnderlineInputBorder(),
+                    labelText: '部屋に入るためのパスワード',
                   ),
-                  SizedBox(width: 70),
-                  ElevatedButton(
-                    onPressed: () {
-                      GoRouter.of(context).pop();
-                    },
-                    child: const Text('Back'),
-                  ),
-                ],
-              ),
-            ],
+                ),
+                _gap,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        audioController.playSfx(SfxType.buttonTap);
+                        GoRouter.of(context).pop();
+                      },
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.grey),
+                      child: const Text('戻る'),
+                    ),
+                    SizedBox(width: 70),
+                    ElevatedButton(
+                      onPressed: () {
+                        audioController.playSfx(SfxType.buttonTap);
+                        GoRouter.of(context).push('/create_room');
+                      },
+                      child: const Text('部屋を作る'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ));
   }
