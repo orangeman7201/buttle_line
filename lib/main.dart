48e766c28ad1e8e2867b6affd9a2f7481a786777
Orangeman7201 <aki.jazz1027@gmail.com>
2023-06-18 01:53:59 +0900

    部屋を作るのボタンの遷移先をcreate_roomに変更

--- lib/main.dart
+++ lib/main.dart
@@ -142,44 +142,12 @@ class MyApp extends StatelessWidget {
               const MainMenuScreen(key: Key('main menu')),
           routes: [
             GoRoute(
-                path: 'play',
-                pageBuilder: (context, state) => buildMyTransition<void>(
-                      child: const LevelSelectionScreen(
-                          key: Key('level selection')),
-                      color: context.watch<Palette>().backgroundLevelSelection,
-                    ),
-                routes: [
-                  GoRoute(
-                    path: 'session/:level',
-                    pageBuilder: (context, state) {
-                      final levelNumber = int.parse(state.params['level']!);
-                      final level = gameLevels
-                          .singleWhere((e) => e.number == levelNumber);
-                      return buildMyTransition<void>(
-                        child: PlaySessionScreen(
-                          level,
-                          key: const Key('play session'),
-                        ),
-                        color: context.watch<Palette>().backgroundPlaySession,
-                      );
-                    },
-                  ),
-                  GoRoute(
-                    path: 'won',
-                    pageBuilder: (context, state) {
-                      final map = state.extra! as Map<String, dynamic>;
-                      final score = map['score'] as Score;
-
-                      return buildMyTransition<void>(
-                        child: WinGameScreen(
-                          score: score,
-                          key: const Key('win game'),
-                        ),
-                        color: context.watch<Palette>().backgroundPlaySession,
-                      );
-                    },
-                  )
-                ]),
+              path: 'create_room',
+              pageBuilder: (context, state) => buildMyTransition<void>(
+                child: const LevelSelectionScreen(key: Key('level selection')),
+                color: context.watch<Palette>().backgroundLevelSelection,
+              ),
+            ),
             GoRoute(
               path: 'settings',
               builder: (context, state) =>
